6aa349b52d31be4bc6f378e1dd24ca6b15c1e8c5
MIS803 <MIS803@30803_PC14>
2021-03-09 16:47:15 +0800

    閏年程式

--- 2-2Practice/Default.aspx.cs
+++ 2-2Practice/Default.aspx.cs
@@ -8,6 +8,32 @@ using System.Web.UI.WebControls;
 namespace _2_2Practice {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
+           
+            int num = 1100;
+            if(num % 4 == 0)
+            {
+                if (num % 100 == 0)
+                {
+                    if (num % 400 == 0)
+                    {
+                        Response.Write("Y");
+                    }
+                    else
+                    {
+                        Response.Write("N");
+                    }
+                }
+                else
+                {
+                    Response.Write("Y");
+                }
+                
+            }else
+                Response.Write("N");
+
+
+
+
 
         }
     }
